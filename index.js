d1c362b321866dee901f5cd385c06d258dbcf84e
traitimtrongvang <locxuandao260520033@gmail.com>
2022-06-28 12:34:48 +0700

    morgan

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const port = 3002
 
+
+app.use(morgan('combined'))
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
